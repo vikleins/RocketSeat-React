b02273f230f265c6268b50ddee7aa7a56f2f828c
Vinicius.Klein <Vinicius.Klein@AEPNBW10036.aeplus.local>
2024-04-29 09:14:00 -0300

    starting home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Header } from './components/Header'
-
+import { Post } from './components/Post'
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from './components/Sidebar'
+
 
 function App() {
 
@@ -8,6 +11,15 @@ function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        < Sidebar />
+        <main>
+          <Post author='vinicius'
+            content="Lorem"></Post>
+        </main>
+
+      </div>
     </>
   )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props) {
+    return (
+        <>
+            <div>{props.author}</div>
+            <div>{props.content}</div>
+        </>
+    )
+}
